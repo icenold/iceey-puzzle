d087f838bdb979154137c986728b0afb5609c3db
zyarnold <arnoldn@zylun.com>
2017-07-21 05:11:30 +0800

    added puzzle component

--- iceey-puzzle/src/app/app.component.ts
+++ iceey-puzzle/src/app/app.component.ts
@@ -1,10 +1,93 @@
-import { Component } from '@angular/core';
+import { EventEmitter,Output,Component, OnInit,Input, ElementRef } from '@angular/core';
+import { PuzzlePiece } from './puzzle-piece';
 
 @Component({
   selector: 'iceey-puzzle',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent  {
+
+  constructor(public elem:ElementRef) { }
+  
+  @Output() onPuzzleMove:EventEmitter<any>;
+  @Input() imgUrl:string;
+  @Input() puzzleLevel:number;
+  @Input() basicDimension:number;
+
+  puzzlePieces:PuzzlePiece[] = [];
+  
+  jsonCorrectPositions:string;
+
+  ngOnInit() {
+    //test
+    this.basicDimension = 225;
+    this.imgUrl = "../download.jpg";
+    this.puzzleLevel = 3;
+    //test
+    this.assignPieces();
+    this.shufflePieces();
+    console.log('pieces',this.puzzlePieces);
+    console.log('puzzleBoard',this.getBoardPossition());
+  }
+
+  private assignPieces(){
+    for(var y = 0; y<this.puzzleLevel; y++){
+      for(var x = 0; x<this.puzzleLevel; x++){
+        var p = new PuzzlePiece();
+        p.correct_x = x;
+        p.correct_y = y;
+        p.current_x = x;
+        p.current_y = y;
+        this.puzzlePieces.push(p);
+      }
+    }
+    this.puzzlePieces[(this.puzzleLevel*this.puzzleLevel)-1].isBlankPiece = true;
+    this.jsonCorrectPositions = JSON.stringify(this.puzzlePieces);
+  }
+  switchWithBlank(clickedPiece:PuzzlePiece){
+    var blankPiece:PuzzlePiece = this.puzzlePieces[this.puzzlePieces.length-1];
+    var sameX = clickedPiece.current_x == blankPiece.current_x && (clickedPiece.current_y+1==blankPiece.current_y || clickedPiece.current_y-1==blankPiece.current_y);
+    var sameY = clickedPiece.current_y == blankPiece.current_y && (clickedPiece.current_x+1==blankPiece.current_x || clickedPiece.current_x-1==blankPiece.current_x);
+    if(sameX || sameY){
+      var blankX = blankPiece.current_x;
+      var blankY = blankPiece.current_y;
+      blankPiece.current_x = clickedPiece.current_x;
+      blankPiece.current_y = clickedPiece.current_y;
+      clickedPiece.current_x = blankX;
+      clickedPiece.current_y = blankY;
+      var isComplete:boolean = this.jsonCorrectPositions==JSON.stringify(this.puzzlePieces);
+      this.onPuzzleMove.emit(isComplete);
+    }
+  }
+  getRelativePosition(index:number, _x:number, _y:number){
+    var x = _x*(this.basicDimension/this.puzzleLevel);
+    var y = _y*(this.basicDimension/this.puzzleLevel);
+    return{
+      x:x,
+      y:y
+    };
+  }
+  public arrangePieces(){
+    this.puzzlePieces=JSON.parse(this.jsonCorrectPositions);
+  }
+  public shufflePieces(){
+    var pieceSetter:PuzzlePiece[] = JSON.parse(JSON.stringify(this.puzzlePieces));
+    //console.log(pieceSetter);
+    for(var ctr=0; ctr<this.puzzlePieces.length; ctr++){
+      var randomIndex = this.randomIntFromInterval(0,pieceSetter.length-1);
+      var removedPiece = pieceSetter.splice(randomIndex,1)[0];
+      this.puzzlePieces[ctr].current_x = removedPiece.correct_x;
+      this.puzzlePieces[ctr].current_y = removedPiece.correct_y;
+    }
+  }
+  getBoardPossition(){
+    return {
+      x:this.elem.nativeElement.querySelector('#puzzleBoard').offsetLeft,
+      y:this.elem.nativeElement.querySelector('#puzzleBoard').offsetTop
+    };
+  }
+  randomIntFromInterval(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+  }
 }
---
+++ iceey-puzzle/src/app/puzzle-piece.ts
@@ -0,0 +1,7 @@
+export class PuzzlePiece {
+    correct_x:number;
+    correct_y:number;
+    current_x:number;
+    current_y:number;
+    isBlankPiece:boolean;
+}
