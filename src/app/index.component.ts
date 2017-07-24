997521a8ecb21ed96c3acb0e7e15272ef1ef9614
zyarnold <arnoldn@zylun.com>
2017-07-24 23:27:34 +0800

    updated demo

--- src/app/index.component.ts
+++ src/app/index.component.ts
@@ -2,9 +2,25 @@ import { EventEmitter,Output,Component, OnInit,Input, ElementRef } from '@angula
 
 @Component({
   selector: 'app-index',
-  styles:[`.description tr td{ border-top:solid 1px black; padding:5px }`],
+  styles:[`.description tr td{ border-top:solid 1px black; padding:3px }`],
   template: `
 
+
+<p>Installation</p>
+<pre><code>npm install iceey-puzzle</code></pre>
+<br>
+<p>Add Iceey Puzzle to your root module</p>
+<pre>
+  <code>
+    {{"import { IceeyPuzzleModule } from 'iceey-puzzle';"}}
+
+    imports: [
+      ...
+      IceeyPuzzleModule
+    ]
+  </code>
+</pre>
+<br>
 <div [style.text-align]="'center'" >
   <iceey-puzzle 
       #puzzle
@@ -16,46 +32,61 @@ import { EventEmitter,Output,Component, OnInit,Input, ElementRef } from '@angula
   </iceey-puzzle>
 </div>
 <br>
+<p>Usage</p>
+<pre><code>{{usage}}</code></pre>
 <table class="description">
   <tr>
     <td>
-      <label>Input: [imgUrl]</label>
+      <pre><code>{{imgUrlUsage}}</code></pre>
+    </td>
+    <td>
       <select [(ngModel)]="settings.imgUrl" >
         <option *ngFor="let i of imageSources" [value]="i" >{{i.replace('/iceey-puzzle','').replace('/assets/','')}}</option>
-      </select>    
+      </select>        
     </td>
   </tr>
   <tr>
     <td>
-      <label>Input: [basicDimension]</label>
-      <input type="number" [(ngModel)]="settings.dimension" min="200" >
+      <pre><code>{{basicDimensionUsage}}</code></pre>
+    </td>
+    <td>
+      <input type="number" [(ngModel)]="settings.dimension" min="200" >      
     </td>
   </tr>
   <tr>
     <td>
-      <label>Input: [puzzleLevel]</label>
+      <pre><code>{{puzzleLevelUsage}}</code></pre>
+    </td>
+    <td>
       <input type="number" [(ngModel)]="settings.level" min="3" >
     </td>
   </tr>
   <tr>
     <td>
-      <label>Input: [delay]</label>
+      <pre><code>{{delayUsage}}</code></pre>
+    </td>
+    <td>
       <input type="number" [(ngModel)]="settings.delay" step="0.1" >
-    </td>
+    </td>    
   </tr>
   <tr>
     <td>
-      Output: (puzzleClick) - Fires an $event. Emits true when the puzzle is completed. See console window
+      <pre><code>{{puzzleClickUsage}}</code></pre>
     </td>
+    <td>Fires $event. Emits true when the puzzle is completed. See console window</td>
   </tr>
   <tr>
     <td>
-      Method: <button (click)="puzzle.shuffle()" >shuffle()</button> - Shuffle puzzle pieces
+      <pre><code>{{shuffleUsage}}</code></pre>
     </td>
+    <td><button (click)="puzzle.shuffle()" >Everyday I'm shufflin</button></td>
   </tr>
   <tr>
     <td>
-      Method: <button (click)="puzzle.arrange()" >arrange()</button> - Arrange puzzle pieces
+      <pre><code>{{arrangeUsage}}</code></pre>
+    </td>
+    <td>
+     <button (click)="puzzle.arrange()" >Arrange it!</button>
     </td>
   </tr>
 </table>
@@ -85,7 +116,16 @@ export class IndexComponent  {
     level:3,
     delay:0.5
   };
-
+  usage = `<iceey-puzzle></iceey-puzzle>`;
+  imgUrlUsage = `<iceey-puzzle [imgUrl]="<string>" ></iceey-puzzle>`;
+  basicDimensionUsage = `<iceey-puzzle [basicDimensionUsage]="<number>" ></iceey-puzzle>`;
+  puzzleLevelUsage = `<iceey-puzzle [puzzleLevel]="<number>" ></iceey-puzzle>`;
+  delayUsage = `<iceey-puzzle [delay]="<number>" ></iceey-puzzle>`;
+  puzzleClickUsage = `<iceey-puzzle (puzzleClick)="doSomething($event)" ></iceey-puzzle>`;
+  shuffleUsage = `<iceey-puzzle #myPuzzle ></iceey-puzzle>
+<button (click)="myPuzzle.shuffle()" >Everyday I'm shufflin<button>`;
+  arrangeUsage = `<iceey-puzzle #myPuzzle ></iceey-puzzle>
+<button (click)="myPuzzle.arrange()" >Arrange it!<button>`;
   // imageSources:any[] = [
   //   "/iceey-puzzle/assets/elephant.jpg",
   //   "/iceey-puzzle/assets/tiger.jpg",
